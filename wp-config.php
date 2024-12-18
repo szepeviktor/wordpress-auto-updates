eab4f9c50832dd5f83820536810ed2a849bb39d5
Viktor Szépe <viktor@szepe.net>
2024-12-18 15:08:08 +0100

    Add link to wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,5 @@
 <?php
 
 // Completely disable all types of automatic updates, core or otherwise
+// https://developer.wordpress.org/advanced-administration/upgrade/upgrading/#configuring-automatic-background-updates
 define('AUTOMATIC_UPDATER_DISABLED', true);
