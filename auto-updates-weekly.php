1e1f191e8b4ca2bc05bdbb18db3867d5681175d8
Viktor Szépe <viktor@szepe.net>
2024-12-18 11:30:02 +0100

    Create auto-updates-weekly.php

---
+++ auto-updates-weekly.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Plugin Name: Reschedule auto-updates to an off-peak hour of the week
+ * Description: Benefit from not installing each version 
+ * Plugin URI: https://github.com/szepeviktor/wordpress-auto-updates
+ */
+
+add_action(
+    'plugins_loaded',
+    static function () {
+        // Originally scheduled "twicedaily"
+        remove_action('init', 'wp_schedule_update_checks');
+        add_action(
+            'init',
+            static function () {
+                $next_run = (int)date('H') < 3 ? strtotime('today 03:10:30') : strtotime('next day 03:10:30');
+                $recurrence = 'weekly';
+                if (! wp_next_scheduled('wp_version_check') && ! wp_installing()) {
+                    wp_schedule_event($next_run, $recurrence, 'wp_version_check');
+                }
+                if (! wp_next_scheduled('wp_update_plugins') && ! wp_installing()) {
+                    wp_schedule_event($next_run, $recurrence, 'wp_update_plugins');
+                }
+                if (! wp_next_scheduled('wp_update_themes') && ! wp_installing()) {
+                    wp_schedule_event($next_run, $recurrence, 'wp_update_themes');
+                }
+            }
+        );
+    },
+    10,
+    0
+);
