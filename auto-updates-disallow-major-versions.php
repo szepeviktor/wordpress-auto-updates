87a5650b73ffdc0c3634c4608427c6a58d28a7a6
Viktor Szépe <viktor@szepe.net>
2024-12-18 11:51:40 +0100

    Create auto-updates-disallow-major-versions.php

---
+++ auto-updates-disallow-major-versions.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Plugin Name: Disallow auto-updates to new major version
+ * Description: Benefit from not installing each version
+ * Plugin URI: https://github.com/szepeviktor/wordpress-auto-updates
+ */
+
+add_filter(
+    'auto_update_plugin',
+    static function ($update, $item) {
+        if (empty($item->response) || $item->response !== 'update') {
+            return $update;
+        }
+
+        $current_major_version = strstr($item->version, '.', true);
+        $new_major_version = strstr($item->new_version, '.', true);
+        if ($new_major_version !== $current_major_version) {
+            return false;
+        }
+        return $update;
+    },
+    10,
+    2
+);
